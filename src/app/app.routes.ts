3d7af173102d626d3dff934ac38f51667289e67a
Bogdanov Daniel <daniel.bogdanov@ict.csbe.ch>
2023-12-02 15:18:13 +0100

    adding login and style for login with required field.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,10 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path:"auth/register",
+    loadComponent:() => import("./pages/auth/register/register.component").then(value => value.RegisterComponent)
+  },
+  {
     path:'auth/login',
     loadComponent: () => import('./pages/auth/login/login.component').then(value => value.LoginComponent)
 
