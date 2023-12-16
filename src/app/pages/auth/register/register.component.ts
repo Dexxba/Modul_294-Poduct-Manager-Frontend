b26464760c32bdd06cdc632494b02fa26b732640
Bogdanov Daniel <daniel.bogdanov@ict.csbe.ch>
2023-12-16 14:06:23 +0100

    add a button and implement the logic: when the 'Register' button is pressed and all parameters are correct, immediately redirect to the 'Login' page and connect the buttons.

--- src/app/pages/auth/register/register.component.ts
+++ src/app/pages/auth/register/register.component.ts
@@ -2,6 +2,8 @@ import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 import {MatInputModule} from "@angular/material/input";
+import {UserControllerService} from "../../../openapi-client";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'pm-register',
@@ -12,7 +14,7 @@ import {MatInputModule} from "@angular/material/input";
 })
 export class RegisterComponent {
   fromGroup = new FormGroup({
-    Email: new FormControl(null, [Validators.pattern(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,10}$/), Validators.required]),
+    email: new FormControl(null, [Validators.pattern(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,10}$/), Validators.required]),
     password: new FormControl(null,Validators.required),
     firstName: new FormControl(null,Validators.required),
     lastName: new FormControl(null,Validators.required),
@@ -21,12 +23,30 @@ export class RegisterComponent {
     city: new FormControl(null,Validators.required),
     country: new FormControl(null,Validators.required),
     mobilePhone: new FormControl(null,Validators.required),
-    email: new FormControl(null,Validators.required),
 
 
   })
 
   submit() {
-
+    console.log(this.fromGroup);
+    if(this.fromGroup.valid) {
+      this.userControllerService.register({
+        email: this.fromGroup.controls.email.value,
+        password: this.fromGroup.controls.password.value,
+        firstName: this.fromGroup.controls.firstName.value,
+        lastName: this.fromGroup.controls.lastName.value,
+        street: this.fromGroup.controls.street.value,
+        zip: this.fromGroup.controls.zip.value,
+        city: this.fromGroup.controls.city.value,
+        country: this.fromGroup.controls.country.value,
+        mobilePhone: this.fromGroup.controls.mobilePhone.value
+      }).subscribe(value => {
+        this.router.navigateByUrl("/auth/login")
+      });
+    }
+  }
+  constructor(
+    private readonly router: Router,
+    private readonly userControllerService:UserControllerService) {
   }
 }
