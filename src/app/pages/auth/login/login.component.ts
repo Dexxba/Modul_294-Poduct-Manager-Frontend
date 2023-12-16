9f581da7f0b484e323fd36c775f3d28cb7931412
Bogdanov Daniel <daniel.bogdanov@ict.csbe.ch>
2023-12-16 14:07:11 +0100

    add a button and implement the logic:'Register'

---
+++ src/app/pages/auth/login/login.component.ts
@@ -0,0 +1,35 @@
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {MatInputModule} from "@angular/material/input";
+import {UserControllerService} from "../../../openapi-client";
+import {RouterLink} from "@angular/router";
+
+@Component({
+  selector: 'pm-login',
+  standalone: true,
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, MatInputModule, RouterLink],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+
+  fromGroup = new FormGroup({
+    email: new FormControl(null, [Validators.pattern(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,10}$/),Validators.required]),
+    password: new FormControl(null,Validators.required)
+  })
+  submit(){
+    console.log(this.fromGroup);
+    if(this.fromGroup.valid){
+      this.userControllerService.login({
+        email: this.fromGroup.controls.email.value,
+        password:this.fromGroup.controls.password.value
+      }).subscribe(value => {
+
+      })
+    }
+  }
+  constructor(private readonly userControllerService :UserControllerService) {
+  }
+
+}
