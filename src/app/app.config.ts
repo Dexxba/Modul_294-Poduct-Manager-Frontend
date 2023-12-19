1cbf26c22ec337ca7d9f6217e5155db641b736e1
Bogdanov Daniel <daniel.bogdanov@ict.csbe.ch>
2023-12-19 17:17:19 +0100

    Setting up the app.config.ts and authorization-interceptor.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,11 +4,15 @@ import {provideRouter} from '@angular/router';
 import {routes} from './app.routes';
 import {provideAnimations} from '@angular/platform-browser/animations';
 import {ApiModule, Configuration} from "./openapi-client";
-import {provideHttpClient} from "@angular/common/http";
+import {provideHttpClient, withInterceptors} from "@angular/common/http";
+import {authorizationInterceptor} from "./pages/auth/interceptor/authorization-interceptor.interceptor";
 
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations(),provideHttpClient(), importProvidersFrom(ApiModule.forRoot(() => {
+  providers: [provideRouter(routes),
+    provideAnimations(),
+    provideHttpClient(withInterceptors([authorizationInterceptor])),
+    importProvidersFrom(ApiModule.forRoot(() => {
     return new Configuration({basePath: 'https://product-manager.cyrotech.ch'})
   }),),]
 };
