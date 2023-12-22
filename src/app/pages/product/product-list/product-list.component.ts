483cf516a0c23feed546036338691749215e70e2
Bogdanov Daniel <daniel.bogdanov@ict.csbe.ch>
2023-12-22 19:46:00 +0100

    Setting up product-list and styling.

--- src/app/pages/product/product-list/product-list.component.ts
+++ src/app/pages/product/product-list/product-list.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {RouterOutlet} from "@angular/router";
+import {RouterLink, RouterOutlet} from "@angular/router";
+import {
+  CategoryControllerService,
+  CategoryShowDto,
+  ProductControllerService,
+  ProductShowDto
+} from "../../../openapi-client";
 
 @Component({
   selector: 'pm-product-list',
   standalone: true,
-    imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterLink],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.scss'
 })
-export class ProductListComponent {
-
+export class ProductListComponent implements OnInit{
+  productShowDto: ProductShowDto[]=[];
+  constructor(private productControllerService:ProductControllerService) {
+  }
+  ngOnInit(): void {
+    this.productControllerService.getAllProducts().subscribe((products)=>{
+        this.productShowDto = products;
+      },
+      (e)=>{console.error(e)});
+  }
 }
