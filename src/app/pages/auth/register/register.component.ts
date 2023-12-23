83da0d510be99079ae4d846a3bf8c4f0ab64b65a
Bogdanov Daniel <daniel.bogdanov@ict.csbe.ch>
2023-12-23 01:56:50 +0100

    Detailed Commenting on register component Code and Modification for Clarity and Easy Reading.

--- src/app/pages/auth/register/register.component.ts
+++ src/app/pages/auth/register/register.component.ts
@@ -1,35 +1,46 @@
-import {Component} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
-import {MatInputModule} from "@angular/material/input";
-import {UserControllerService} from "../../../openapi-client";
-import {Router} from "@angular/router";
+// Importing Angular core and common modules.
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
+// Importing form control modules for reactive forms, along with validators.
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from "@angular/forms";
+
+// Importing Angular Material input module for UI elements.
+import { MatInputModule } from "@angular/material/input";
+
+// Importing a service for user control, possibly for handling user registration.
+import { UserControllerService } from "../../../openapi-client";
+
+// Importing Angular's Router module for navigation.
+import { Router } from "@angular/router";
+
+// Component decorator to define metadata for the RegisterComponent.
 @Component({
-  selector: 'pm-register',
-  standalone: true,
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, MatInputModule],
-  templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  selector: 'pm-register', // The custom HTML tag for this component.
+  standalone: true, // Indicates that the component is standalone.
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, MatInputModule], // Modules required for this component.
+  templateUrl: './register.component.html', // Link to the HTML template for this component.
+  styleUrl: './register.component.scss' // Link to the stylesheet for this component.
 })
 export class RegisterComponent {
+  // FormGroup to manage and validate form data.
   fromGroup = new FormGroup({
     email: new FormControl(null, [Validators.pattern(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,10}$/), Validators.required]),
-    password: new FormControl(null,Validators.required),
-    firstName: new FormControl(null,Validators.required),
-    lastName: new FormControl(null,Validators.required),
-    street: new FormControl(null,Validators.required),
-    zip: new FormControl(null,Validators.required),
-    city: new FormControl(null,Validators.required),
-    country: new FormControl(null,Validators.required),
-    mobilePhone: new FormControl(null,Validators.required),
-
-
+    password: new FormControl(null, Validators.required),
+    firstName: new FormControl(null, Validators.required),
+    lastName: new FormControl(null, Validators.required),
+    street: new FormControl(null, Validators.required),
+    zip: new FormControl(null, Validators.required),
+    city: new FormControl(null, Validators.required),
+    country: new FormControl(null, Validators.required),
+    mobilePhone: new FormControl(null, Validators.required),
   })
 
+  // Method to handle form submission.
   submit() {
-    console.log(this.fromGroup);
-    if(this.fromGroup.valid) {
+    console.log(this.fromGroup); // Logging the form group for debugging purposes.
+    if (this.fromGroup.valid) {
+      // If the form is valid, call the register method from the userControllerService.
       this.userControllerService.register({
         email: this.fromGroup.controls.email.value,
         password: this.fromGroup.controls.password.value,
@@ -41,12 +52,15 @@ export class RegisterComponent {
         country: this.fromGroup.controls.country.value,
         mobilePhone: this.fromGroup.controls.mobilePhone.value
       }).subscribe(value => {
+        // After successful registration, navigate to the login page.
         this.router.navigateByUrl("/auth/login")
       });
     }
   }
+
+  // Constructor for dependency injection.
   constructor(
     private readonly router: Router,
-    private readonly userControllerService:UserControllerService) {
-  }
+    private readonly userControllerService: UserControllerService
+  ) { }
 }
