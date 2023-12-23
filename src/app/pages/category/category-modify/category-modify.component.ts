6d80933cc41b2bc5318802439e5b74a1073b6333
Bogdanov Daniel <daniel.bogdanov@ict.csbe.ch>
2023-12-23 01:53:24 +0100

    Detailed Commenting on category-modify Code and Modification for Clarity and Easy Reading.

--- src/app/pages/category/category-modify/category-modify.component.ts
+++ src/app/pages/category/category-modify/category-modify.component.ts
@@ -1,36 +1,52 @@
+// Importing Angular core modules.
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
-import {MatFormFieldModule} from "@angular/material/form-field";
-import {Router, RouterLink} from "@angular/router";
-import {CategoryControllerService, } from "../../../openapi-client";
 
+// Importing modules for handling forms in Angular.
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from "@angular/forms";
+
+// Importing Angular Material form field module for UI components.
+import { MatFormFieldModule } from "@angular/material/form-field";
+
+// Importing Angular's Router modules for navigation.
+import { Router, RouterLink } from "@angular/router";
+
+// Importing the CategoryControllerService from a client library, presumably for API interactions.
+import { CategoryControllerService } from "../../../openapi-client";
+
+// Component decorator to define metadata for the CategoryModifyComponent.
 @Component({
-  selector: 'pm-category-modify',
-  standalone: true,
-  imports: [CommonModule, FormsModule, MatFormFieldModule, ReactiveFormsModule, RouterLink],
-  templateUrl: './category-modify.component.html',
-  styleUrl: './category-modify.component.scss'
+  selector: 'pm-category-modify', // The custom HTML tag for this component.
+  standalone: true, // Indicates that the component is standalone.
+  imports: [CommonModule, FormsModule, MatFormFieldModule, ReactiveFormsModule, RouterLink], // Modules required for this component.
+  templateUrl: './category-modify.component.html', // Link to the HTML template for this component.
+  styleUrl: './category-modify.component.scss' // Link to the stylesheet for this component.
 })
 export class CategoryModifyComponent {
+  // FormGroup to handle form data.
   fromGroup = new FormGroup({
-    name: new FormControl(null, Validators.required),
-    active:new FormControl(null, Validators.required),
-
+    name: new FormControl(null, Validators.required), // FormControl for the category name.
+    active: new FormControl(null, Validators.required), // FormControl for the 'active' status.
   })
+
+  // Method to handle form submission.
   submit() {
-    console.log(this.fromGroup);
-    if(this.fromGroup.valid) {
+    console.log(this.fromGroup); // Logging the form group for debugging.
+    if (this.fromGroup.valid) {
+      // If the form is valid, call the createCategory method with form data.
       this.categoryControllerService.createCategory({
         name: this.fromGroup.controls.name.value,
-        active:this.fromGroup.controls.active.value
+        active: this.fromGroup.controls.active.value
       }).subscribe(value => {
+        // Navigate to the category list page after successful creation.
         this.router.navigateByUrl("/category/list")
       });
     }
   }
+
+  // Constructor for dependency injection.
   constructor(
-    private readonly router: Router,
-    private readonly categoryControllerService:CategoryControllerService) {
-  }
+    private readonly router: Router, // Injecting the Router for navigation.
+    private readonly categoryControllerService: CategoryControllerService // Injecting the service for category management.
+  ) {}
 }
