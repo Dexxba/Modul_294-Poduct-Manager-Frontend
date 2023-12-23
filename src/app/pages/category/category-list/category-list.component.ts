35e8a49f6d2b1da0b6ad918565ad4a8f8097ab3f
Bogdanov Daniel <daniel.bogdanov@ict.csbe.ch>
2023-12-23 01:52:51 +0100

    Detailed Commenting on category-list Code and Modification for Clarity and Easy Reading.

--- src/app/pages/category/category-list/category-list.component.ts
+++ src/app/pages/category/category-list/category-list.component.ts
@@ -1,30 +1,49 @@
-import { Component,OnInit } from '@angular/core';
+// Importing necessary Angular core modules and decorators.
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {CategoryControllerService, CategoryShowDto} from "../../../openapi-client";
-import {Router, RouterLink} from "@angular/router";
 
+// Importing services and DTOs (Data Transfer Objects) from an OpenAPI client.
+import { CategoryControllerService, CategoryShowDto } from "../../../openapi-client";
+
+// Importing Angular Router modules for navigation.
+import { Router, RouterLink } from "@angular/router";
+
+// Component decorator to define metadata for the CategoryListComponent.
 @Component({
-  selector: 'pm-category-list',
-  standalone: true,
-  imports: [CommonModule, RouterLink],
-  templateUrl: './category-list.component.html',
-  styleUrl: './category-list.component.scss'
+  selector: 'pm-category-list', // The custom HTML tag for this component.
+  standalone: true, // Indicates that the component is standalone.
+  imports: [CommonModule, RouterLink], // Modules required for this component.
+  templateUrl: './category-list.component.html', // Link to the HTML template for this component.
+  styleUrl: './category-list.component.scss' // Link to the stylesheet for this component.
 })
-export class CategoryListComponent  implements OnInit{
-  categoryShowDto: CategoryShowDto[]=[];
-  constructor(private categoryControllerService:CategoryControllerService,private router: Router,) {
+export class CategoryListComponent implements OnInit {
+  // Property to hold category data.
+  categoryShowDto: CategoryShowDto[] = [];
+
+  // Constructor for dependency injection.
+  constructor(
+    private categoryControllerService: CategoryControllerService, // Service for category management.
+    private router: Router // Angular Router for navigation.
+  ) {}
+
+  // ngOnInit lifecycle hook for component initialization.
+  ngOnInit(): void {
+    // Calling the getAllCategories method from the service to fetch category data.
+    this.categoryControllerService.getAllCategories().subscribe(
+      (categories) => {
+        this.categoryShowDto = categories; // Assigning the fetched categories to the property.
+      },
+      (e) => { console.error(e) } // Error handling.
+    );
   }
 
-  ngOnInit(): void {
-    this.categoryControllerService.getAllCategories().subscribe((categories)=>{
-    this.categoryShowDto = categories;
-    },
-      (e)=>{console.error(e)});
-  }
-  delete(Id:number) {
-      this.categoryControllerService.deleteCategoryById(Id)
-      .subscribe(value => {
-        this.ngOnInit();
-      });
+  // Method to handle category deletion.
+  delete(Id: number) {
+    // Calling the deleteCategoryById method from the service.
+    this.categoryControllerService.deleteCategoryById(Id).subscribe(
+      value => {
+        this.ngOnInit(); // Re-initializing component to refresh the category list.
+      }
+    );
   }
 }
