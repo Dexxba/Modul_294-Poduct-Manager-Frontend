389c35685edbc8b1b952bf266b820978157ae488
Bogdanov Daniel <daniel.bogdanov@ict.csbe.ch>
2023-12-23 01:55:40 +0100

    Detailed Commenting on product-modify component Code and Modification for Clarity and Easy Reading.

--- src/app/pages/product/product-modify/product-modify.component.ts
+++ src/app/pages/product/product-modify/product-modify.component.ts
@@ -1,18 +1,29 @@
+// Importing necessary Angular core modules and decorators.
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
-import {Router, RouterLink} from "@angular/router";
-import {CategoryControllerService, ProductControllerService} from "../../../openapi-client";
-import {MatFormFieldModule} from "@angular/material/form-field";
 
+// Importing modules for handling forms in Angular.
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from "@angular/forms";
+
+// Importing Angular Router modules for navigation.
+import { Router, RouterLink } from "@angular/router";
+
+// Importing services from an OpenAPI client, presumably for API interactions.
+import { ProductControllerService } from "../../../openapi-client";
+
+// Importing Angular Material form field module for UI components.
+import { MatFormFieldModule } from "@angular/material/form-field";
+
+// Component decorator to define metadata for the ProductModifyComponent.
 @Component({
-  selector: 'pm-product-modify',
-  standalone: true,
-  imports: [CommonModule, FormsModule, MatFormFieldModule, RouterLink, ReactiveFormsModule],
-  templateUrl: './product-modify.component.html',
-  styleUrl: './product-modify.component.scss'
+  selector: 'pm-product-modify', // The custom HTML tag for this component.
+  standalone: true, // Indicates that the component is standalone.
+  imports: [CommonModule, FormsModule, MatFormFieldModule, RouterLink, ReactiveFormsModule], // Modules required for this component.
+  templateUrl: './product-modify.component.html', // Link to the HTML template for this component.
+  styleUrl: './product-modify.component.scss' // Link to the stylesheet for this component.
 })
 export class ProductModifyComponent {
+  // FormGroup to handle form data.
   fromGroup = new FormGroup({
     sku: new FormControl(null, Validators.required),
     active: new FormControl(null, Validators.required),
@@ -23,9 +34,12 @@ export class ProductModifyComponent {
     stock: new FormControl(null, Validators.required),
     categoryId: new FormControl(null, Validators.required),
   })
+
+  // Method to handle form submission.
   submit() {
-    console.log(this.fromGroup);
-    if(this.fromGroup.valid) {
+    console.log(this.fromGroup); // Logging the form group for debugging.
+    if (this.fromGroup.valid) {
+      // If the form is valid, call the createProduct method with form data.
       this.productControllerService.createProduct({
         sku: this.fromGroup.controls.sku.value,
         active: this.fromGroup.controls.active.value,
@@ -36,12 +50,15 @@ export class ProductModifyComponent {
         stock: this.fromGroup.controls.stock.value,
         categoryId: this.fromGroup.controls.categoryId.value
       }).subscribe(value => {
+        // Navigate to the product list page after successful creation/modification.
         this.router.navigateByUrl("/product/list")
       });
     }
   }
+
+  // Constructor for dependency injection.
   constructor(
-    private readonly router: Router,
-    private readonly productControllerService:ProductControllerService) {
-  }
+    private readonly router: Router, // Injecting the Router for navigation.
+    private readonly productControllerService: ProductControllerService // Injecting the service for product management.
+  ) {}
 }
