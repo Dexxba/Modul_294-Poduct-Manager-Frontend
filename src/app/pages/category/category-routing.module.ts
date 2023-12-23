8205441c0a49a1cc5d7255b2616e42c5ea23a958
Bogdanov Daniel <daniel.bogdanov@ict.csbe.ch>
2023-12-23 01:53:47 +0100

    Detailed Commenting on category-routing Code and Modification for Clarity and Easy Reading.

--- src/app/pages/category/category-routing.module.ts
+++ src/app/pages/category/category-routing.module.ts
@@ -1,25 +1,40 @@
+// Importing necessary Angular modules.
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {adminStrongGuard} from "../auth/guard/admin-strong.guard";
 
+// Importing the adminStrongGuard for route guarding.
+import { adminStrongGuard } from "../auth/guard/admin-strong.guard";
+
+// Defining the routes for the category features.
 const routes: Routes = [
   {
-    path:"list",
-    loadComponent: () => import("./category-list/category-list.component").then(value => value.CategoryListComponent)
+    // Route for listing categories.
+    path: "list",
+    // Dynamically loading the CategoryListComponent when the route is visited.
+    loadComponent: () => import("./category-list/category-list.component")
+      .then(value => value.CategoryListComponent)
   },
   {
-    path:":id",
-    loadComponent:() => import("./category-modify/category-modify.component").then(value => value.CategoryModifyComponent)
+    // Route for viewing category details. ':id' is a route parameter.
+    path: "details/:id",
+    // Dynamically loading the CategoryModifyComponent for this route.
+    loadComponent: () => import("./category-modify/category-modify.component")
+      .then(value => value.CategoryModifyComponent)
   },
   {
-    path:"edit/create",
-    loadComponent:() => import("./category-modify/category-modify.component").then(value => value.CategoryModifyComponent),
-    canActivate:[adminStrongGuard]
+    // Route for creating a new category.
+    path: "edit/create",
+    // Dynamically loading the CategoryModifyComponent for the creation route.
+    loadComponent: () => import("./category-modify/category-modify.component")
+      .then(value => value.CategoryModifyComponent),
+    // Protecting this route with the adminStrongGuard.
+    canActivate: [adminStrongGuard]
   }
 ];
 
+// NgModule decorator to define metadata for the routing module.
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forChild(routes)], // Importing RouterModule with child routes.
+  exports: [RouterModule] // Exporting RouterModule for use in other parts of the application.
 })
 export class CategoryRoutingModule { }
